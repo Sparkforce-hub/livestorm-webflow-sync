a9ed74dc0079b07ac6cca7972e02c0cf81b801c6
Sparkforce-hub <sara@sparkforce.dk>
2026-03-15 17:59:32 +0100

    Update sync-all.js

--- api/sync-all.js
+++ api/sync-all.js
@@ -1,4 +1,4 @@
- export default async function handler(req, res) {
+export default async function handler(req, res) {
     if (req.method !== "GET" && req.method !== "POST") {                        
       return res.status(405).send("Method not allowed");
     }
